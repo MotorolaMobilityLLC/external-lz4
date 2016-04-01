2c1c0e11ba728fdb163d11514310bbd4ed089304
David Riley <davidriley@google.com>
2016-04-01 11:05:08 -0700

    Make lz4 host executable use statically linked C++ library.
    Change-Id: I1529355f9a13fb8a400f84b0ffa77e1d9e3e34ac

--- programs/Android.mk
+++ programs/Android.mk
@@ -6,5 +6,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := lz4
 LOCAL_SRC_FILES := bench.c lz4io.c lz4cli.c
 LOCAL_STATIC_LIBRARIES := liblz4
+LOCAL_CXX_STL := libc++_static
 include $(BUILD_HOST_EXECUTABLE)
 
